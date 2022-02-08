46e90c859520889cdabd2ef68a8ea0b502125503
Samuel Fransson <94680334+SamFrans@users.noreply.github.com>
2022-02-08 11:34:53 +0100

    Update domjs.js

--- js/domjs.js
+++ js/domjs.js
@@ -17,5 +17,5 @@ function showDiv(n) {
         for (i = 0; i < imageArray.length; i++) {
         imageArray[i].style.display = "none";
         }
-        imageArray [imageCounter - 1].style.display = "block";             
+        imageArray[imageCounter - 1].style.display = "block";             
 }
